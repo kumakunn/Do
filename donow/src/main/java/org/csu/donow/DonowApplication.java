d832a07fd3adf8db4abb570e9726e7e13d616103
shuxin <1025565665@qq.com>
2018-09-05 14:54:52 +0800

    first commit.initialize the project

---
+++ donow/src/main/java/org/csu/donow/DonowApplication.java
@@ -0,0 +1,12 @@
+package org.csu.donow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DonowApplication.class, args);
+    }
+}
---
+++ donow/src/test/java/org/csu/donow/DonowApplicationTests.java
@@ -0,0 +1,16 @@
+package org.csu.donow;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DonowApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
